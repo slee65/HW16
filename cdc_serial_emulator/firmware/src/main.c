c4fa7e362ae1fa700190e58c84170462533fe07a
slee65 <stephenlee2016@u.northwestern.edu>
2015-06-04 15:30:52 -0500

    Final Project

---
+++ cdc_serial_emulator/firmware/src/main.c
@@ -0,0 +1,125 @@
+/*******************************************************************************
+  MPLAB Harmony Project Main Source File
+
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for an MPLAB Harmony project.
+
+  Description:
+    This file contains the "main" function for an MPLAB Harmony project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state 
+    machines of all MPLAB Harmony modules in the system and it calls the 
+    "SYS_Tasks" function from within a system-wide "super" loop to maintain 
+    their correct operation. These two functions are implemented in 
+    configuration-specific files (usually "system_init.c" and "system_tasks.c")
+    in a configuration-specific folder under the "src/system_config" folder 
+    within this project's top-level folder.  An MPLAB Harmony project may have
+    more than one configuration, each contained within it's own folder under
+    the "system_config" folder.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+//Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "system/common/sys_module.h"
+#include "app.h"// SYS function prototypes
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all MPLAB Harmony modules, including application(s). */
+    SYS_Initialize ( NULL );
+    TRISBbits.TRISB7=0;
+LATBbits.LATB7=1;
+
+//TRISBbits.TRISB5=0;
+//LATBbits.LATB5=1;
+RPB5Rbits.RPB5R=0b0101;
+
+
+TRISBbits.TRISB14=0;
+LATBbits.LATB14=1;
+// set up LED2 as OC1 using Timer2 at 1kHz Pin B15
+
+
+ANSELBbits.ANSB15=0;
+RPB15Rbits.RPB15R=0b0101;
+OC1CONbits.OCTSEL=0;
+OC1CONbits.OCM = 0b110;
+
+OC2CONbits.OCTSEL=0;
+OC2CONbits.OCM = 0b110;
+//OC1RS=20000;
+
+//OC1RS=20000;
+OC1R=20000;
+//OC2RS=20000;
+OC2R=20000;
+//timer 2
+PR2=19999;
+TMR2=0;
+T2CONbits.TCKPS=0b001;
+T2CONbits.ON=1;
+OC1CONbits.ON = 1;
+OC2CONbits.ON = 1;
+ _CP0_SET_COUNT(0);
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
